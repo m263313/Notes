1c001b97d943f12ef3312238cae72eaeee3c0634
m263313 <m263313@gmail.com>
2016-12-14 12:39:01 +0200

    Changes minor

--- app/src/main/java/com/androshchuk/notes/MainPageActivity.java
+++ app/src/main/java/com/androshchuk/notes/MainPageActivity.java
@@ -1,5 +1,6 @@
 package com.androshchuk.notes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainPageActivity extends AppCompatActivity {
 
@@ -18,14 +20,14 @@ public class MainPageActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
@@ -48,5 +50,10 @@ public class MainPageActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void addNote(View view){
+        Intent intent = new Intent(this, NewNoteActivity.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/androshchuk/notes/NewNoteActivity.java
@@ -0,0 +1,13 @@
+package com.androshchuk.notes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewNoteActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_note);
+    }
+}
