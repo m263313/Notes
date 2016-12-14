17863cdd4dd37cdafbca572fe60d79c94bf26d60
m263313 <m263313@gmail.com>
2016-12-14 23:46:51 +0200

    Fix changes of activities

--- app/src/main/java/com/androshchuk/notes/NewNoteActivity.java
+++ app/src/main/java/com/androshchuk/notes/NewNoteActivity.java
@@ -1,7 +1,11 @@
 package com.androshchuk.notes;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class NewNoteActivity extends Activity {
 
@@ -9,5 +13,18 @@ public class NewNoteActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note);
+
+
+        Intent intent = getIntent();
+
+
+
+    }
+    public void addNoteDatabase(View view){
+        Intent intent = new Intent(this,  ShowAllActivity.class);
+
+        startActivity(intent);
+
+
     }
 }
